b7df7b1e366b0352f859cac2c5bb639c3ece66b0
Margarida Nogueira <margarida.nogueira.05@gmail.com>
2026-01-20 14:45:40 +0000

    Correções

--- Historia.py
+++ Historia.py
@@ -6,9 +6,9 @@ nome = input("Insira o nome do personagem principal: ")
 lugar = input("Insira o local onde a história se passa: ")
 adjetivo = input("Insira um adjetivo para descrever o personagem: ")
 verbo = input("Insira um verbo sobre que o personagem faz: ")
-tempo = input("Insira o tempo em que a história se passa: ", end="\n\n")
+tempo = input("Insira o tempo em que a história se passa: ")
 
-print("A criar a sua história...", end="\n\n")
+print("\n\nA criar a sua história...", end="\n\n")
 
 print("Uma História em", lugar, sep="**", end="\n")
 print("Era uma vez um(a)", adjetivo," chamado(a)", nome, end=" ")

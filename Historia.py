70a23e215f8ccb231d6709c66b554a5d258a8be6
Margarida Nogueira <margarida.nogueira.05@gmail.com>
2026-01-20 12:07:51 +0000

    Correção 1 primeira parte

--- Historia.py
+++ Historia.py
@@ -2,10 +2,10 @@ print(" * " * 10)
 print("     Criador de Histórias     ")
 print(" * " * 10, end = " ")
 
-nome = input("Insira o nome do personagem principal: ")
+nome = input("\n\nInsira o nome do personagem principal: ")
 lugar = input("Insira o local onde a história se passa: ")
 adjetivo = input("Insira um adjetivo para descrever o personagem: ")
 verbo = input("Insira um verbo sobre que o personagem faz: ")
 tempo = input("Insira o tempo em que a história se passa: ")
 
-print("A criar a sua história...")
+print("\n\nA criar a sua história...")
